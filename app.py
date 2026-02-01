2f74a14a6cd922b2d5509702e39c4ec1eec8d244
jftigre <joaofelipetigre@gmail.com>
2026-02-01 02:39:58 -0300

    Refatorar estrutura do banco de dados e modelo de usuário

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "coxinha123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
-
+db.init_app(app)
+#Session <- conexão ativa
 
 if __name__ == '__main__':
     app.run(debug=True)
